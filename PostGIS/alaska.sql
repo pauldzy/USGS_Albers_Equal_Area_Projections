e186d0325148904fe2bc6957517ec9875ebc3af7
Paul Dziemiela <paul@dziemiela.com>
2016-01-01 15:47:42 -0500

    initial draft

---
+++ PostGIS/alaska.sql
@@ -0,0 +1,14 @@
+INSERT INTO spatial_ref_sys(
+    srid
+   ,auth_name
+   ,auth_srid
+   ,proj4text
+   ,srtext
+) VALUES (
+    3338
+   ,'NAD83 Alaska Albers Equal Area USGS'
+   ,3338
+   ,'+proj=aea +lat_1=55 +lat_2=65 +lat_0=50 +lon_0=-154 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs '
+   ,'PROJCS["NAD83 Alaska Albers Equal Area USGS",GEOGCS["GCS_North_American_1983",DATUM["North_American_Datum_1983",SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["False_Easting",0],PARAMETER["False_Northing",0],PARAMETER["longitude_of_center",-154],PARAMETER["Standard_Parallel_1",55],PARAMETER["Standard_Parallel_2",65],PARAMETER["latitude_of_center",50],UNIT["Meter",1],AUTHORITY["EPA OW","990003"]]'
+);
+
