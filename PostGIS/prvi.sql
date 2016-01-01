36f4d80bda498bea18cde042b12c3275b3b1520e
Paul Dziemiela <paul@dziemiela.com>
2016-01-01 15:48:28 -0500

    initial draft

---
+++ PostGIS/prvi.sql
@@ -0,0 +1,14 @@
+INSERT INTO spatial_ref_sys(
+    srid
+   ,auth_name
+   ,auth_srid
+   ,proj4text
+   ,srtext
+) VALUES (
+    990004
+   ,'NAD83 Puerto Rico/Virgin Islands Albers Equal Area USGS'
+   ,990004
+   ,'+proj=aea +lat_1=8 +lat_2=18 +lat_0=3 +lon_0=-66 +x_0=0 +y_0=0 +ellps=GRS80 +datum=NAD83 +units=m +no_defs '
+   ,'PROJCS["NAD83 Puerto Rico/Virgin Islands Albers Equal Area USGS",GEOGCS["GCS_North_American_1983",DATUM["North_American_Datum_1983",SPHEROID["GRS_1980",6378137,298.257222101]],PRIMEM["Greenwich",0],UNIT["Degree",0.017453292519943295]],PROJECTION["Albers_Conic_Equal_Area"],PARAMETER["False_Easting",0],PARAMETER["False_Northing",0],PARAMETER["longitude_of_center",-66],PARAMETER["Standard_Parallel_1",8],PARAMETER["Standard_Parallel_2",18],PARAMETER["latitude_of_center",3],UNIT["Meter",1],AUTHORITY["EPA OW","990004"]]'
+);
+
